cea90db0eb2012e6f69625b798e30220038ec0a1
aarahman <arif@themascoteers.com>
2023-11-20 13:51:55 +1100

    done

--- algorithms/fscr/fscr.py
+++ algorithms/fscr/fscr.py
@@ -30,45 +30,42 @@ class FSCR:
         weight_decay = self.lr/10
         return torch.optim.Adam(self.model.parameters(), lr=self.lr, weight_decay=weight_decay)
 
-    def fit(self, X_train, y_train):
-        self.original_feature_size = X_train.shape[1]
+    def fit(self, X, y):
+        self.original_feature_size = X.shape[1]
         self.write_columns()
         self.model.train()
         optimizer = self.create_optimizer()
-        X_train = torch.tensor(X_train, dtype=torch.float32).to(self.device)
-        y_train = torch.tensor(y_train, dtype=torch.float32).to(self.device)
-        size = X_train.shape[0]
-        spline = get_splines(X_train)
+        X  = torch.tensor(X , dtype=torch.float32).to(self.device)
+        y = torch.tensor(y, dtype=torch.float32).to(self.device)
+        size = X.shape[0]
+        spline = get_splines(X)
         for epoch in range(self.epochs):
             y_hat = self.model(spline, size)
-            loss = self.criterion(y_hat, y_train)
+            loss = self.criterion(y_hat, y)
             for machine in self.model.machines:
                 loss = loss + machine.range_loss()
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
-            row = self.dump_row(epoch)
+            row = self.dump_row(epoch, X, y)
             print("".join([str(i).ljust(20) for i in row]))
         return self.model
 
-    def evaluate(self,dataset):
-        batch_size = 30000
-        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+    def evaluate(self,X,y):
         self.model.eval()
-        for (x, y) in dataloader:
-            x = x.to(self.device)
-            y = y.to(self.device)
-            y_hat = self.model(x)
-            y_hat = y_hat.reshape(-1)
-            y_hat = y_hat.detach().cpu().numpy()
-            y = y.detach().cpu().numpy()
-            r2 = r2_score(y, y_hat)
-            rmse = math.sqrt(mean_squared_error(y, y_hat))
-            self.model.train()
-            return max(r2,0), rmse
+        size = X.shape[0]
+        spline = get_splines(X)
+        y_hat = self.model(spline, size)
+        y_hat = y_hat.reshape(-1)
+        y_hat = y_hat.detach().cpu().numpy()
+        y = y.detach().cpu().numpy()
+        r2 = r2_score(y, y_hat)
+        rmse = math.sqrt(mean_squared_error(y, y_hat))
+        self.model.train()
+        return max(r2,0), rmse
 
-    def train_results(self):
-        return self.evaluate(self.train_dataset)
+    def train_results(self, X, y):
+        return self.evaluate(X, y)
 
     def write_columns(self):
         columns = ["epoch","train_r2","train_rmse","time"]
@@ -79,8 +76,8 @@ class FSCR:
             file.write(",".join(columns))
             file.write("\n")
 
-    def dump_row(self, epoch):
-        train_r2, train_rmse = self.train_results()
+    def dump_row(self, epoch, X, y):
+        train_r2, train_rmse = self.train_results(X, y)
         row = [train_r2, train_rmse]
         row = [round(r,5) for r in row]
         row = [epoch] + row + [self.get_elapsed_time()]
