82473100f6981400afd4d54aee98fb656091f120
Arif <arif@themascoteers.com>
2023-11-20 22:34:06 +1100

    done

--- algorithm_creator.py
+++ algorithm_creator.py
@@ -6,7 +6,6 @@ from algorithms.algorithm_sfs import AlgorithmSFS
 from algorithms.algorithm_sbs import AlgorithmSBS
 from algorithms.algorithm_kbest import AlgorithmKBest
 from algorithms.algorithm_fm import AlgorithmFM
-from algorithms.algorithm_vr import AlgorithmVT
 
 
 class AlgorithmCreator:
@@ -28,5 +27,4 @@ class AlgorithmCreator:
             return AlgorithmFM(X_train, y_train, target_feature_size)
         elif name == "kbest":
             return AlgorithmKBest(X_train, y_train, target_feature_size)
-        elif name == "vt":
-            return AlgorithmVT(X_train, y_train, target_feature_size)
+
