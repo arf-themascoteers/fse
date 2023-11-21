396885bec7be7250d5696c0f5f6d58ee137c8c0c
Arif <arif@themascoteers.com>
2023-11-22 00:05:09 +1100

    done

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 from evaluator import Evaluator
 
 if __name__ == '__main__':
-    #short_size = [5,10,15,20,25,30]
-    big_size = [10,50,100,150,200,250,300,350]
+    sizes = [5,10,15,20,25,30]
+    #sizes = [10, 50, 100, 150, 200, 250, 300, 350]
     tasks = []
-    tasks.append(
-        {
-            "reduced_features": False,
-            "reduced_rows": False,
-            "target_feature_size": 10,
-            "algorithm": "pcat95"
-        }
-    )
-    for size in big_size:
+    for size in sizes:
         for algorithm in ["fscrns","fscr"]:
             tasks.append(
                 {
-                    "reduced_features":False,
-                    "reduced_rows":False,
+                    "reduced_features":True,
+                    "reduced_rows":True,
                     "target_feature_size": size,
                     "algorithm": algorithm
                 }
