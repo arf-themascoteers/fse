4f523732160f281d204108147b5c3fd3cdaaba36
Arif <arif@themascoteers.com>
2023-11-21 18:44:28 +1100

    done

--- main.py
+++ main.py
@@ -4,14 +4,23 @@ if __name__ == '__main__':
     #short_size = [5,10,15,20,25,30]
     big_size = [10,50,100,150,200,250,300,350]
     tasks = []
+    tasks.append(
+        {
+            "reduced_features": False,
+            "reduced_rows": False,
+            "target_feature_size": 10,
+            "algorithm": "pcat95"
+        }
+    )
     for size in big_size:
-        tasks.append(
-            {
-                "reduced_features":False,
-                "reduced_rows":False,
-                "target_feature_size": size,
-                "algorithm": "fscr"
-            }
-        )
+        for algorithm in ["pca","fscr"]:
+            tasks.append(
+                {
+                    "reduced_features":False,
+                    "reduced_rows":False,
+                    "target_feature_size": size,
+                    "algorithm": algorithm
+                }
+            )
     ev = Evaluator(tasks)
     ev.evaluate()
