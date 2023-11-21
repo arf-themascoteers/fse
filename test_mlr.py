7795248fe6011ec6a14043335fd0e1f5f11c258a
Arif <arif@themascoteers.com>
2023-11-21 18:41:38 +1100

    done

--- test_mlr.py
+++ test_mlr.py
@@ -2,7 +2,7 @@ from sklearn.linear_model import LinearRegression
 from ds_manager import DSManager
 
 
-d = DSManager()
+d = DSManager(reduced_features=False, reduced_rows=False)
 mlr = LinearRegression()
 trainx, trainy, testx, testy = d.get_train_test_X_y()
 mlr.fit(trainx, trainy)
