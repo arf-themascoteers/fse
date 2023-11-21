54084b2fe2986398b8ba15391612e55819637ad9
Arif <arif@themascoteers.com>
2023-11-21 18:28:19 +1100

    done

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from evaluator import Evaluator
 
 if __name__ == '__main__':
-    short_size = [5,10,15,20,25,30]
-    big_size = [10,100,200,250,1000,2000]
+    #short_size = [5,10,15,20,25,30]
+    big_size = [10,50,100,150,200,250,300,350]
     tasks = []
     for size in big_size:
         tasks.append(
--- my_utils.py
+++ my_utils.py
@@ -1,44 +1,35 @@
 from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
 from model_ann import ModelANN
 import torch.nn as nn
-import torch
 
 
 def get_hidden_for_full(feature_size):
     h1 = 50
     h2 = 10
-    if feature_size >=50 and feature_size < 100:
+    if 50 <= feature_size < 100:
         h1 = 40
-    elif feature_size >=100 and feature_size <= 200:
+    elif 100 <= feature_size < 200:
         h1 = 30
-    elif feature_size >200 and feature_size < 1000:
-        h1 = 10
-    elif feature_size >= 1000 and feature_size < 2000:
-        h1 = 8
-        h2 = 0
-    elif feature_size >= 2000 and feature_size < 3000:
-        h1 = 4
-        h2 = 0
-    elif feature_size >= 3000:
-        h1 = 0
-        h2 = 0
+    elif 200 <= feature_size < 300 :
+        h1 = 20
+    elif 300 <= feature_size:
+        h1 = 18
     return h1, h2
 
 
 def get_hidden_for_short(feature_size):
     h1 = 15
     h2 = 10
-    if feature_size >=50 and feature_size < 100:
+    if 50 <= feature_size < 100:
         h1 = 11
-    elif feature_size >=100 and feature_size <= 200:
+    elif 100 <= feature_size < 200:
         h1 = 6
         h2 = 5
-    elif feature_size > 200 and feature_size <= 250:
+    elif 200 <= feature_size < 250:
         h1 = 5
         h2 = 4
-    elif feature_size > 300:
-        h1 = 0
+    elif 250 < feature_size:
+        h1 = 5
         h2 = 0
     return h1, h2
 
