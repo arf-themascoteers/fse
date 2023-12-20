17c0d213a78cffabeac5737d45e3e8a732ff887d
aarahman <arif@themascoteers.com>
2023-12-20 12:13:30 +1100

    done

--- algorithms/algorithm_lasso.py
+++ algorithms/algorithm_lasso.py
@@ -10,7 +10,7 @@ class AlgorithmLasso(Algorithm):
         self.indices = None
 
     def get_selected_indices(self):
-        lasso = Lasso(alpha=1.0)
+        lasso = Lasso(alpha=0.001)
         lasso.fit(self.X_train, self.y_train)
         self.indices = np.argsort(np.abs(lasso.coef_))[::-1][:self.target_feature_size]
         return self, self.indices
