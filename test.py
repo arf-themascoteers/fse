8a79aac455b094181a42dd6de4f4b53ba34dc4a6
arif <arif@themascoteers.com>
2024-04-11 15:46:51 +1000

    done

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ from evaluator import Evaluator
 
 if __name__ == '__main__':
     tasks = []
-    for dataset in ["lucas_full","brazilian","lucas_min"]:
+    #for dataset in ["lucas_full","brazilian","lucas_skipped"]:
+    for dataset in ["lucas_skipped"]:
         for algorithm in ["fsdr","mi","lasso"]:
             for size in [5,10,20]:
                 tasks.append(
