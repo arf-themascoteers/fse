a459fb541d4844dd4e57965383b401d4c95f3d01
arif <arif@themascoteers.com>
2024-04-12 17:34:23 +1000

    done

--- test_compe.py
+++ test_compe.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     tasks = []
     for dataset in ["brazilian","lucas_skipped","lucas_full"]:
         for size in [2, 5, 10, 20, 30]:
-            for algorithm in ["cars","fsdr"]:
+            for algorithm in ["spa"]:
                 tasks.append(
                     {
                         "dataset": dataset,
