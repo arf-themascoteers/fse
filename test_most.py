5f4498671e395e580698f6053758b191f2564b0f
arif <arif@themascoteers.com>
2024-04-20 13:40:02 +1000

    done

---
+++ test_most.py
@@ -0,0 +1,16 @@
+from evaluator import Evaluator
+
+if __name__ == '__main__':
+    tasks = []
+    for algorithm in ["mcuve", "cars", "spa", "rf"]:
+        for dataset in ["brazilian","lucas_skipped","lucas_full"]:
+            for size in [2, 5, 10, 20, 30]:
+                tasks.append(
+                    {
+                        "dataset": dataset,
+                        "target_feature_size": size,
+                        "algorithm": algorithm
+                    }
+                )
+    ev = Evaluator(tasks)
+    ev.evaluate()
