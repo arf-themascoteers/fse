01153e601aaf2ff881005c6c0045b92e1e4e4430
arif <arifcs@gmail.com>
2024-04-25 16:42:07 +1000

    don

--- test_reproc.py
+++ test_reproc.py
@@ -2,9 +2,9 @@ from evaluator import Evaluator
 
 if __name__ == '__main__':
     tasks = {
-        "algorithms" : ["fsdr", "fsdrd"],
-        "datasets" : ["lucas_skipped_min"],
+        "algorithms" : ["fsdr","fsdrd"],
+        "datasets" : ["lucas_full"],
         "target_sizes" : [10]
     }
-    ev = Evaluator(tasks,3,1,"fsdr.csv")
+    ev = Evaluator(tasks,10, 1,"fsdr.csv")
     ev.evaluate()
