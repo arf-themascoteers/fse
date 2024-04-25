a41b018b128ac2687e550180b1857cdfa159d0d8
Arif <arif@themascoteers.com>
2024-04-25 23:35:44 +1000

    done

--- test_fsdr.py
+++ test_fsdr.py
@@ -3,7 +3,7 @@ from evaluator import Evaluator
 if __name__ == '__main__':
     tasks = {
         "algorithms" : ["fsdr"],
-        "datasets" : ["brazilian", "lucas_skipped", "lucas_full"],
+        "datasets" : ["lucas_skipped", "lucas_full"],
         "target_sizes" : [5, 10, 15, 20, 25, 30]
     }
     ev = Evaluator(tasks,1,1,"fsdr.csv")
