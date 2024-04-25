b001f2879578e518cbc450838f2db9aa8c8583ad
Arif <arif@themascoteers.com>
2024-04-26 01:07:53 +1000

    done

--- test_fsdr_vs_bsnet.py
+++ test_fsdr_vs_bsnet.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
         "datasets" : ["lucas_skipped", "lucas_full", "lucas_min"],
         "target_sizes" : [5, 10, 15, 20, 25, 30]
     }
-    ev = Evaluator(tasks,1,5,"fsdr_vs_bsnet.csv")
+    ev = Evaluator(tasks,5,5,"fsdr_vs_bsnet.csv")
     ev.evaluate()
