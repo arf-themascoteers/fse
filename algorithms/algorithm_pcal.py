8df2a9ddc62ad0433a093c7cd8d3fdb17666e12f
Arif <arif@themascoteers.com>
2024-04-25 21:53:04 +1000

    done

---
+++ algorithms/algorithm_pcal.py
@@ -0,0 +1,18 @@
+from algorithm import Algorithm
+from sklearn.linear_model import Lasso
+import numpy as np
+
+
+class AlgorithmPCALoading(Algorithm):
+    def __init__(self, target_feature_size, X_train, y_train, X_validation, y_validation):
+        super().__init__(target_feature_size, X_train, y_train, X_validation, y_validation)
+        self.indices = None
+
+    def get_selected_indices(self):
+        lasso = Lasso(alpha=0.001)
+        lasso.fit(self.X_train, self.y_train)
+        self.indices = np.argsort(np.abs(lasso.coef_))[::-1][:self.target_feature_size]
+        return self, self.indices
+
+    def transform(self, X):
+        return X[:,self.indices]
---
+++ test_pcaloading.py
@@ -0,0 +1,15 @@
+from evaluator import Evaluator
+
+if __name__ == '__main__':
+    tasks = {
+        "algorithms" : ["mcuve", "cars", "lasso", "rf"],
+        "datasets" : ["brazilian", "lucas_skipped", "lucas_full"],
+        "target_sizes" : [5, 10, 15, 20, 25, 30]
+    }
+    tasks = {
+        "algorithms" : ["mi","lasso"],
+        "datasets" : ["lucas_downsampled_min", "lucas_skipped_min"],
+        "target_sizes" : [2, 5]
+    }
+    ev = Evaluator(tasks,3,4,"quick.csv")
+    ev.evaluate()
