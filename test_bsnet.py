8892839a88114d160311331bd6ccf53e44888b84
Arif <arif@themascoteers.com>
2024-04-26 17:58:05 +1000

    done

--- test_bsnet.py
+++ test_bsnet.py
@@ -2,9 +2,9 @@ from evaluator import Evaluator
 
 if __name__ == '__main__':
     tasks = {
-        "algorithms" : ["bsnet"],
+        "algorithms" : ["bsnet","fsdr"],
         "datasets" : ["indian_pines"],
         "target_sizes" : [5, 10, 15, 20, 25, 30]
     }
-    ev = Evaluator(tasks,1,3,"bsnet.csv")
+    ev = Evaluator(tasks,3,1,"bsnet.csv")
     ev.evaluate()
