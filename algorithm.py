343948dc1b959ec495dbd03e5f2fd514e614d53c
Arif <arif@themascoteers.com>
2024-04-26 19:27:11 +1000

    Ignored

--- algorithm.py
+++ algorithm.py
@@ -7,6 +7,7 @@ class Algorithm(ABC):
         self.target_feature_size = target_feature_size
         self.selected_indices = []
         self.model = None
+        self.all_indices = None
 
     def fit(self):
         self.model, self.selected_indices = self.get_selected_indices()
@@ -20,3 +21,16 @@ class Algorithm(ABC):
     @abstractmethod
     def get_selected_indices(self):
         pass
+
+    @abstractmethod
+    def get_name(self):
+        pass
+
+    def get_all_indices(self):
+        return self.all_indices
+
+    def _set_all_indices(self, all_indices):
+        self.all_indices = all_indices
+
+    def is_independent_of_target_size(self):
+        return self.get_all_indices() is not None
