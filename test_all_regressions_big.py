095a7f819d8a9e4d0da093c63a853f3394f25400
arif <arifcs@gmail.com>
2024-04-29 22:06:00 +1000

    don

--- test_all_regressions_big.py
+++ test_all_regressions_big.py
@@ -2,7 +2,7 @@ from evaluator import Evaluator
 
 if __name__ == '__main__':
     tasks = {
-        "algorithms" : ["fsdr","bsnet","spa","mcuve","pcal","lasso"],
+        "algorithms" : ["fsdrl","bsnet","spa","mcuve","pcal","lasso"],
         "datasets" : ["lucas_full", "indian_pines", "ghsi", "lucas_skipped", "lucas_downscaled"],
         "target_sizes" : [5, 10, 15, 20, 25, 30]
     }
