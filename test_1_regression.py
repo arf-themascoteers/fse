9c05371f804cb0226b89576a8402e6d3c26b61d1
Arif <arif@themascoteers.com>
2024-05-01 06:58:49 +1000

    Ignored

--- test_1_regression.py
+++ test_1_regression.py
@@ -3,8 +3,8 @@ from evaluator import Evaluator
 if __name__ == '__main__':
     tasks = {
         "algorithms" : ["fsdrl","bsnet","mcuve","pcal","lasso"],
-        "datasets" : ["lucas_full", "indian_pines", "ghsi", "lucas_skipped", "lucas_downscaled", "lucas_min"],
+        "datasets" : ["lucas_full","lucas_skipped","lucas_downsampled","lucas_min"],
         "target_sizes" : [5, 10, 15, 20, 25, 30]
     }
-    ev = Evaluator(tasks,1,10,"fsdr.csv")
+    ev = Evaluator(tasks,1,10,"test_1_regression.csv")
     ev.evaluate()
