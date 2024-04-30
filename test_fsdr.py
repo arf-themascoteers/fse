bee958e9490770112698a609f705f3ab71305b3d
Arif <arif@themascoteers.com>
2024-04-30 21:32:40 +1000

    Ignored

--- test_fsdr.py
+++ test_fsdr.py
@@ -2,9 +2,9 @@ from evaluator import Evaluator
 
 if __name__ == '__main__':
     tasks = {
-        "algorithms" : ["fsdr"],
-        "datasets" : ["lucas_skipped", "lucas_full"],
+        "algorithms" : ["fsdrl"],
+        "datasets" : ["lucas_full"],
         "target_sizes" : [5, 10, 15, 20, 25, 30]
     }
-    ev = Evaluator(tasks,1,1,"fsdr.csv")
+    ev = Evaluator(tasks,1,10,"fsdr.csv")
     ev.evaluate()
