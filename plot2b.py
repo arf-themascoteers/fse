38cd514c0c9a6e3350605336b3c1be31dc1511bd
Arif <arif@themascoteers.com>
2024-05-01 08:01:28 +1000

    done

--- plot2b.py
+++ plot2b.py
@@ -5,9 +5,20 @@ df = pd.read_csv("b22.csv")
 
 fig = px.scatter(df, x='band1', y='band2',
                     color='score',
-                    color_continuous_scale='Viridis',
-                    title='3D Scatter Plot',
-                    opacity=1
+                    color_continuous_scale='ylgnbu',
+                    labels={"band1": "Band Index 1", "band2": "Band Index 2", "score": "Score"}
                     )
+
+fig.update_layout({
+    'plot_bgcolor': 'white',
+    'paper_bgcolor': 'white',
+    'title_x': 0.5
+})
+
+fig.update_layout(
+    font=dict(size=22),
+
+)
+
 
 fig.write_image("fig1.png", scale=5)
