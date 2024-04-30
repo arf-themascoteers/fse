5ae41f6eacd47225b405c91d29e4ee1e5994a102
Arif <arif@themascoteers.com>
2024-04-30 17:19:40 +1000

    done

---
+++ result_manual.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+
+def remove_mcuve():
+    summary = 'saved\\1_all_ex_spa\\fsdr.csv'
+    details = 'saved\\1_all_ex_spa\\details_fsdr.csv'
+
+    summary_df = pd.read_csv(summary)
+    details_df = pd.read_csv(details)
+
+    summary_df = summary_df[ (summary_df["algorithm"] != "mcuve") | (summary_df["dataset"] != "lucas_full")]
+    summary_df.to_csv(summary, index=False)
+
+    details_df = details_df[(details_df["algorithm"] != "mcuve") | (details_df["dataset"] != "lucas_full")]
+    details_df.to_csv(details, index=False)
+
+
+def remove_mcuve2():
+    summary = 'saved\\1_mcuve_spa\\mcuve_luc.csv'
+    details = 'saved\\1_mcuve_spa\\details_mcuve_luc.csv'
+
+    summary_df = pd.read_csv(summary)
+    details_df = pd.read_csv(details)
+
+    summary_df = summary_df[ (summary_df["algorithm"] != "mcuve") | (summary_df["dataset"] != "lucas_min")]
+    summary_df.to_csv(summary, index=False)
+
+    details_df = details_df[(details_df["algorithm"] != "mcuve") | (details_df["dataset"] != "lucas_min")]
+    details_df.to_csv(details, index=False)
+
+
+def remove_lucasmin():
+    summary = 'saved\\1_all_ex_spa\\fsdr.csv'
+    details = 'saved\\1_all_ex_spa\\details_fsdr.csv'
+
+    summary_df = pd.read_csv(summary)
+    details_df = pd.read_csv(details)
+
+    summary_df = summary_df[ (summary_df["algorithm"] != "mcuve") | (summary_df["dataset"] != "lucas_min")]
+    summary_df.to_csv(summary, index=False)
+
+    details_df = details_df[(details_df["algorithm"] != "mcuve") | (details_df["dataset"] != "lucas_min")]
+    details_df.to_csv(details, index=False)
+
--- result_merger.py
+++ result_merger.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import os
 
-locations = ["saved/1_all_ex_spa","saved/1_bsnet_lucas_min","saved/1_mcuve_spa"]
 df = None
+root = "saved"
+locations = [os.path.join(root, subfolder) for subfolder in os.listdir(root) if subfolder.startswith("1_")]
+
 
 for loc in locations:
     files = os.listdir(loc)
@@ -15,28 +17,15 @@ for loc in locations:
             continue
         p = os.path.join(loc, f)
         d = pd.read_csv(p)
+        d['source'] = p
         if df is None:
             df = d
         else:
             df = pd.concat([df, d], axis=0)
 
-#6
+
 algorithms = ["fsdrl","bsnet","mcuve","pcal","lasso", "spa"]
-
-#6
 datasets = ["lucas_full","lucas_skipped","lucas_downsampled","lucas_min","indian_pines", "ghsi"]
-# for al in algorithms:
-#     lasso = df[df["algorithm"] == al]
-#     print(al,len(lasso))
-#
-# counter = 0
-# for al in datasets:
-#     lasso = df[df["dataset"] == al]
-#     print(al,len(lasso))
-#     counter = counter + len(lasso)
-#
-
-#6
 targets = [5,10,15,20,25,30]
 
 for d in datasets:
@@ -44,6 +33,8 @@ for d in datasets:
         for t in targets:
             entries = df[ (df["algorithm"] == a) & (df["dataset"] == d) & (df["target_size"] == t)]
             if len(entries) == 0:
-                print(f"Missing {d} {t} {a}")
+                print(f"------Missing {d} {t} {a}")
+                pass
             if len(entries) > 1:
-                print(f"Multiple {d} {t} {a}")
+                print(f"Multiple {d} {t} {a} -- {len(entries)}: {list(entries['source'])}")
+                pass
