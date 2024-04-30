0979e1958ba5c329bb43527960694fa7dcbe29dc
Arif <arif@themascoteers.com>
2024-05-01 07:36:21 +1000

    done

--- plot2b3d.py
+++ plot2b3d.py
@@ -6,5 +6,7 @@ df = pd.read_csv("b22.csv")
 fig = px.scatter_3d(df, x='band1', y='band2', z='score',
                     color='score',
                     color_continuous_scale='Viridis',
-                    title='3D Scatter Plot')
+                    title='3D Scatter Plot',
+                    opacity=0.5
+                    )
 fig.show()
