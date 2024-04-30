412407b11ba7087856a1bc6404f72d9862679fad
arif <arifcs@gmail.com>
2024-04-30 19:37:09 +1000

    don

--- test_all_spa_luc.py
+++ test_all_spa_luc.py
@@ -3,8 +3,8 @@ from evaluator import Evaluator
 if __name__ == '__main__':
     tasks = {
         "algorithms" : ["spa"],
-        "datasets" : ["lucas_min"],
-        "target_sizes" : [20, 25, 30]
+        "datasets" : ["lucas_full"],
+        "target_sizes" : [5,10,15, 20, 25, 30]
     }
     ev = Evaluator(tasks,1,1,"luc2.csv")
     ev.evaluate()
