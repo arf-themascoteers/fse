acd2743c5099991867cc876e4fbf401cd5ac3118
Arif <arif@themascoteers.com>
2024-05-02 03:59:03 +1000

    done

--- signal_plot.py
+++ signal_plot.py
@@ -1,14 +1,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-lucas = pd.read_csv("data/lucas_down_min.csv")
-data = lucas.iloc[120,13:].to_numpy()
+lucas = pd.read_csv("data/lucas_downsampled_min.csv")
+data = lucas.iloc[120,1:].to_numpy()
 x = list(range(data.shape[0]))
-plt.plot(x,data)
+#plt.figure(figsize=(8, 3))
+plt.scatter(x,data, marker=".")
+plt.xticks([])
+plt.yticks([])
+plt.xlabel("Band Index", fontsize=18)
+plt.ylabel("Reflectance", fontsize=18)
 plt.show()
 
-# lucas = pd.read_csv("data/brazilian.csv")
-# data = lucas.iloc[50,1:].to_numpy()
-# x = list(range(data.shape[0]))
 # plt.plot(x,data)
-# plt.show()
+# plt.xticks([])
+# plt.yticks([])
+# plt.gca().spines['top'].set_visible(False)
+# plt.gca().spines['right'].set_visible(False)
+# plt.gca().spines['bottom'].set_visible(False)
+# plt.gca().spines['left'].set_visible(False)
+
+plt.show()
