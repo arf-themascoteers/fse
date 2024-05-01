75dbf55e9f1d8a6d7acc14eb76145063d763a480
Arif <arif@themascoteers.com>
2024-05-02 06:44:03 +1000

    done

--- plotters/band_change.py
+++ plotters/band_change.py
@@ -3,26 +3,33 @@ import plotly.express as px
 import plotly.graph_objects as go
 import os
 
+root = "../saved/0_1"
+locs = [os.path.join(root, sub) for sub in os.listdir(root) if sub.startswith("fscrl-lucas_full-5-")]
+for idx,loc in enumerate(locs):
+    df = pd.read_csv(loc)
+    band_labels = []
+    for i in range(1,6):
+        band_labels.append(f"band_{i}")
+    band_labels = ["epoch"] + band_labels
+    df = df[band_labels]
+    limit = 4000
+    df = df[df["epoch"]<4000]
 
-df = pd.read_csv("../results/fscrl-indian_pines-5-1714482682207401.csv")
-band_labels = []
-for i in range(1,6):
-    band_labels.append(f"band_{i}")
-band_labels = ["epoch"] + band_labels
-df = df[band_labels]
-limit = 4000
-df = df[df["epoch"]<700]
+    fig = px.line(df, x='epoch', y="band_1",labels={"epoch": "Epoch", "band_1": "Band Index 1"})
 
-fig = px.line(df, x='epoch', y="band_1",labels={"epoch": "Epoch", "band_1": "Band Index 1"})
+    for i in range(2,6):
+        additional_trace = go.Scatter(x=df["epoch"], y=df[f"band_{i}"], mode='lines', name=f'Band Index {i}')
+        fig.add_trace(additional_trace)
 
-for i in range(2,6):
-    additional_trace = go.Scatter(x=df["epoch"], y=df[f"band_{i}"], mode='lines', name=f'Band Index {i}')
-    fig.add_trace(additional_trace)
+    fig.update_layout({
+        'plot_bgcolor': 'white',
+        'paper_bgcolor': 'white',
+        'title_x': 0.5
+    })
 
-fig.update_layout({
-    'plot_bgcolor': 'white',
-    'paper_bgcolor': 'white',
-    'title_x': 0.5
-})
-fig.show()
-fig.write_image(f"../saved_figs/band_update_{limit}_epochs_IP.png", scale=5)
+    subfolder = os.path.join("../saved_figs", "bands")
+    if not os.path.exists(subfolder):
+        os.mkdir(subfolder)
+    path = os.path.join(subfolder, f"{idx}.png")
+
+    fig.write_image(path, scale=5)
---
+++ plotters/bar_r2.py
@@ -0,0 +1,52 @@
+import os.path
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+
+
+def plot_bar():
+    root = "../saved_figs"
+    df_original = pd.read_csv(f"../final_results/regression.csv")
+    df = df_original[df_original["algorithm"] != "All Bands"].copy()
+    algorithms = ['BSDR', 'BS-Net-FC', 'MCUVE', 'PCA-loading', 'LASSO', 'SPA']
+    df_lucas = df[(df["dataset"] == "LUCAS") & (df["target_size"] == 5)]
+    df_short = df[(df["dataset"] == "LUCAS (Truncated)") & (df["target_size"] == 5)]
+
+    lucas_time = []
+    short_time = []
+
+    for a in algorithms:
+        rows = df_lucas[df_lucas["algorithm"] == a]
+        lucas_time.append(rows.iloc[0]["time"])
+
+        rows = df_short[df_short["algorithm"] == a]
+        short_time.append(rows.iloc[0]["time"])
+
+    fig = go.Figure(data=[
+        go.Bar(name='LUCAS (Truncated)', x=algorithms, y=short_time),
+        go.Bar(name='LUCAS', x=algorithms, y=lucas_time)
+    ])
+
+    fig.update_yaxes(showticklabels=False)
+
+    fig.update_layout(
+        barmode='group',
+        xaxis_title='Algorithm',
+        yaxis_title='$R^2$',
+    )
+
+    fig.update_layout({
+        'plot_bgcolor': 'white',
+        'paper_bgcolor': 'white',
+        'title_x': 0.5
+    })
+
+    subfolder = os.path.join(root, "bars")
+    if not os.path.exists(subfolder):
+        os.mkdir(subfolder)
+    path = os.path.join(subfolder,f"r2.png")
+    fig.write_image(path, scale=5)
+
+
+
+plot_bar()
