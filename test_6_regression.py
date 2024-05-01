ae4d63bb36b333dc0d25e82cf07765fda926619d
Arif <arif@themascoteers.com>
2024-05-02 00:54:29 +1000

    done

---
+++ test_6_regression.py
@@ -0,0 +1,10 @@
+from evaluator import Evaluator
+
+if __name__ == '__main__':
+    tasks = {
+        "algorithms" : ["fsdrl","bsnet","mcuve","pcal","lasso"],
+        "datasets" : ["lucas_skipped","lucas_downsampled","lucas_min"],
+        "target_sizes" : [5, 10, 15, 20, 25, 30]
+    }
+    ev = Evaluator(tasks,1,10,"test_6_regression.csv")
+    ev.evaluate()
