a0e83435197e72231dce7641df98eaa6ff9b1a93
Arif <arif@themascoteers.com>
2024-05-02 14:27:38 +1000

    done

--- model_param_test.py
+++ model_param_test.py
@@ -2,9 +2,9 @@ from evaluator import Evaluator
 
 if __name__ == '__main__':
     tasks = {
-        "algorithms" : ["fsdrl","bsnet"],
-        "datasets" : ["lucas_full","lucas_downsampled"],
+        "algorithms" : ["fsdrl","bsnet","zhang"],
+        "datasets" : ["ghsi","indian_pines"],
         "target_sizes" : [5]
     }
-    ev = Evaluator(tasks,1,1,"dummy.csv")
+    ev = Evaluator(tasks,1,1,"dummy2.csv")
     ev.evaluate()
